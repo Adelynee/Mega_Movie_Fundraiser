81f6fa92bf7bf6ed402f2ffc841353b1631313a6
Adelynee <101477867+Adelynee@users.noreply.github.com>
2022-03-15 12:17:20 +1300

    Create 00_MMF_base_v1.py

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,28 @@
+# import statements
+
+
+# functions go here
+
+#  ----   Main Routine   -----
+
+# Set up dictionaries / Lists needed to gold data
+
+# Ask user if they have used the program before & show instructions if necessary
+
+# Loop to get ticket details
+
+    # Get name (can't be blank)
+
+    # Get age (between 12 and 130)
+
+    # Calculate ticket price
+
+    # Loop to ask for snacks
+
+    # Calculate snack price
+
+    # Ask for payment method ( and apply surchage if necesary )
+
+# Calculate Total sales and profit
+
+# Output data to text files
