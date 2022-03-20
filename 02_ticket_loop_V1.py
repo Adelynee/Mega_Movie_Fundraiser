2685fdc87a49351c9051c23bf2ce59cf605bb11e
Adelynee <101477867+Adelynee@users.noreply.github.com>
2022-03-21 10:50:07 +1300

    Update 02_ticket_loop_V1.py

--- 02_ticket_loop_V1.py
+++ 02_ticket_loop_V1.py
@@ -5,7 +5,7 @@ name = ""
 count = 0
 MAX_TICKETS = 5
 
-while name != "xxx" and count < MAX_TICKETS:
+while name != "xxx" and count <= MAX_TICKETS:
 
     # Get details...
     name = input("Name: ")
