9a10f897ed5a45cbf6ddbce5ee7f76a481af5f9d
Adelynee <101477867+Adelynee@users.noreply.github.com>
2022-03-21 10:15:23 +1300

    Update 00_MMF_base_v1.py

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -3,6 +3,31 @@
 
 # functions go here
 
+# checks that ticket name is not blank
+
+def not_blank(question, error_message):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        # If name in not blank, program continues
+
+        if response != "":
+            return response
+
+        # If name is not blank, show error ($ repeat loop)
+
+        else:
+            print(error_message)
+
+
+# Main Routine goes here
+
+name = not_blank("Name: ", "Sorry - this can't be blank, "
+                           "please enter your name")
+
+
 #  ----   Main Routine   -----
 
 # Set up dictionaries / Lists needed to gold data
@@ -12,6 +37,8 @@
 # Loop to get ticket details
 
     # Get name (can't be blank)
+
+    name = not_blank("Name: ")
 
     # Get age (between 12 and 130)
 
