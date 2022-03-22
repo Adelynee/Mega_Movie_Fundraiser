31613ae1a29665eab068b6cded01723689b6eab3
Adelynee <101477867+Adelynee@users.noreply.github.com>
2022-03-23 12:56:44 +1300

    Update 03_number_checker_V2.py

--- 03_number_checker_V2.py
+++ 03_number_checker_V2.py
@@ -1,19 +1,42 @@
-# functions goes here
+# function goes here
 
 
-# Checks for an intger more than 0
-def int_check(question):
-
+# Checks for an integer more than 0
+def int_check(question) :
+    print("test")
     error = "Please enter a whole number that is more than 0"
 
     valid = False
     while not valid:
 
-            # ask user for number nd check it is valid
-            try:
-                response = int(input(question))
+        # ask user for number and check it is valid
+        try:
+            response = int(input(question))
 
-                if response <= 0:
+            if response <= 0:
+                print(error)
+            else:
+                return response
+        except ValueError:
+            print(error)
+
+def int_check(question, low_num, high_num):
+
+            error = "Please enter a whole number between {} " \
+                    "and {}".format(low_num, high_num)
+
+            valid = False
+            while not valid:
+
+                # ask user for a number and check it is valid
+                try:
+                    response = int(input(question))
+
+                    if low_num <= response <= high_num:
+                        return response
+                    else:
+                        print(error)
+
+                # if an integer is nt entered, display an error
+                except ValueError:
                     print(error)
-                else:
-                    return response
