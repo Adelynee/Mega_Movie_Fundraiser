07a1f7e1f56c015c0d4e8a1ebe490a92e987a93f
Adelynee <101477867+Adelynee@users.noreply.github.com>
2022-03-29 13:29:18 +1300

    Update 00_MMF_base_v1.py

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -53,15 +53,18 @@ def int_check(question):
 # Start of loop
 
 # Initialise loop so that it runs at least once
-name = ""
-count = 0
 MAX_TICKETS = 5
 
-while name != "xxx" and count < MAX_TICKETS:
+name = ""
+ticket_count = 0
+ticket_sales = 0
+
+
+while name != "xxx" and ticket_count < MAX_TICKETS:
 
     # tells user how many seats are left
-    if count < 4:
-        print("You have {} seats left".format(MAX_TICKETS - count))
+    if ticket_count < MAX_TICKETS - 1:
+        print("You have {} seats left".format(MAX_TICKETS - ticket_count))
 
     # Warns user that one seat is left!
     else:
@@ -88,13 +91,26 @@ while name != "xxx" and count < MAX_TICKETS:
         print("That is very old - it looks like  mistake")
         continue
 
-    count += 1
+    if age < 16:
+        ticket_price = 7.5
+    elif age < 65:
+        ticket_price = 10.5
+    else:
+        ticket_price = 6.5
 
-if count == MAX_TICKETS:
+    ticket_count += 1
+    ticket_sales += ticket_price
+
+# End of ticket loop
+# Calculate ticket profit...
+ticket_profit = ticket_sales - (5 * ticket_count)
+print("Ticket profit: ${:.2f}".format(ticket_profit))
+
+if ticket_count == MAX_TICKETS:
     print("You have sold all the available tickets!")
 else:
     print("You have sold {} tickets. \n"
-            "There are {} places still available".format(count, MAX_TICKETS - count))
+            "There are {} places still available".format(ticket_count, MAX_TICKETS - ticket_count))
 
 
     # Get age (between 12 and 130)
