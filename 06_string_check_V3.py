271f1e533b5ef6ea03c1d14593eb4d35ba32af72
Adelynee <101477867+Adelynee@users.noreply.github.com>
2022-04-06 10:45:06 +1200

    Update 06_string_check_V3.py

--- 06_string_check_V3.py
+++ 06_string_check_V3.py
@@ -5,10 +5,10 @@ def string_check(choice, options):
 
     for var_list in options:
 
-        # if the snack is in the one of the lists, return the full nale
+        # if the snack is in the one of the lists, return the full name
         if choice in var_list:
 
-            # Get full name of snacl and put it
+            # Get full name of snack and put it
             # in title case os it looks nice when outputted
             chosen = var_list[0].title()
             is_valid = "yes"
